e16d5be262d15482ec82653a53315b4eeed84a29
Bárbara Ferreira <barbara9ferreira@gmail.com>
2023-05-19 18:56:30 +0100

    função jogo

--- interface.py
+++ interface.py
@@ -51,20 +51,48 @@ def tela_menu(estado):
     pygame.display.set_caption("Jogo dos Semáforos")
     janela_menu.blit(menu2,(0,0))
 
+    ## ativar Botao regras
+    botaoregras = pygame.Rect((50,457),(312,98))
+
+    ## ativar botao Sair
+    botaoSair = pygame.Rect((423, 646),(312,98))
+
+    ## ativar botao BOT
+    botaoBot = pygame.Rect((423, 457),(312,98))
+
+    ## ativar Botao 1vs1
+    botao1v1 = pygame.Rect((804, 457),(312,98))
+
+
     while estado == "Menu":
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if botaoregras.collidepoint(pygame.mouse.get_pos()):
+                    estado = "Regras"
+                if botaoBot.collidepoint(pygame.mouse.get_pos()):
+                    estado = "Bot"
+                if botao1v1.collidepoint(pygame.mouse.get_pos()):
+                    estado = "1v1"
+                if botaoSair.collidepoint(pygame.mouse.get_pos()):
+                    pygame.quit
+                    quit()
         pygame.display.update()
 
         return estado
+    
 
-def tela_jogbot(estado):
+
+def tela_bot(estado):
     janela_bot = pygame.display.set_mode((1150,800))
     pygame.display.set_caption("Jogo dos Semáforos")
     janela_bot.blit(bot2,(0,0))
+
+    botaovoltar2 = pygame.Rect((261, 528),(312,98))
+    botaoplay1 = pygame.Rect((605,528),(312,98))
+
     nome_jogador1 = ""
 
     while estado == "Bot":
@@ -72,6 +100,13 @@ def tela_jogbot(estado):
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if botaoplay1.collidepoint(pygame.mouse.get_pos()):
+                    estado = "JogoB"
+                if botaovoltar2.collidepoint(pygame.mouse.get_pos()):
+                    estado = "Menu"
+
 
             elif event.type == pygame.KEYDOWN:
                            
@@ -94,11 +129,20 @@ def tela_jogbot(estado):
 
         janela_bot.blit(texto_jogador1, posicao_texto_jogador1)
       
-
-    
         pygame.display.update()
 
         
+def tela_JogoB(estado):
+    janela_bot = pygame.display.set_mode((1150,800))
+    pygame.display.set_caption("Jogo dos Semáforos")
+    janela_bot.blit(jogo2,(0,0))
+
+    while estado == "JogoB":
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        pygame.display.update()
 
 
 
@@ -109,4 +153,6 @@ def tela_jogbot(estado):
 #         estado = tela_menu(estado)
 
 # estado = "Bot"
-# tela_jogbot(estado)
+# tela_bot(estado)
+estado = "JogoB"
+tela_JogoB(estado)
